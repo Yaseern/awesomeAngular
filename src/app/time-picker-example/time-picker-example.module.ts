dc9b6d9db3ef4eb7bb7028dd68ce5dfda3943b05
Yasir Munas <ymu@tiqri.com>
2021-03-29 00:28:19 +0530

    updated demo for latest version plugin

--- src/app/time-picker-example/time-picker-example.module.ts
+++ src/app/time-picker-example/time-picker-example.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { TimePickerExampleComponent } from './time-picker-example.component';
 import { RouterModule } from '@angular/router';
 import { TimePickerExampleRoute } from './time-picker-example.routing.module';
 import { NgTimePickerModule } from 'ngy-time-picker'
-
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [TimePickerExampleComponent],
   imports: [
     RouterModule.forChild(TimePickerExampleRoute),
-    NgTimePickerModule
+    NgTimePickerModule,
+    FormsModule
   ]
 })
 export class TimePickerExampleModule { }
